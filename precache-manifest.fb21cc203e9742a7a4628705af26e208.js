b02ef16b44ec9b4db0771a01d46e29fed410674d
rituneeta <rituneeta@gmail.com>
2020-09-13 16:40:53 +0530

    Updates

---
+++ precache-manifest.fb21cc203e9742a7a4628705af26e208.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37d287be2525f70907bc315bb094df23",
+    "url": "/tik-tok-clone/index.html"
+  },
+  {
+    "revision": "60deaad4a47f6fb269d0",
+    "url": "/tik-tok-clone/static/css/main.9b0dafd3.chunk.css"
+  },
+  {
+    "revision": "6683fd33c4e5a272cc6e",
+    "url": "/tik-tok-clone/static/js/2.7759e5f4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/tik-tok-clone/static/js/2.7759e5f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60deaad4a47f6fb269d0",
+    "url": "/tik-tok-clone/static/js/main.fa6127cf.chunk.js"
+  },
+  {
+    "revision": "a15aff5662f63e81035b",
+    "url": "/tik-tok-clone/static/js/runtime-main.f5422934.js"
+  },
+  {
+    "revision": "fe3e8945894c5e142c2a7561fae20090",
+    "url": "/tik-tok-clone/static/media/vedio1.fe3e8945.mp4"
+  },
+  {
+    "revision": "75450777208a7cef85c15dbe08325aa6",
+    "url": "/tik-tok-clone/static/media/vedio2.75450777.mp4"
+  }
+]);
